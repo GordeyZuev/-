21a8196006d775d158798cece7b983a7f29a0c1a
Gordey Zuev <58434126+GordeyZuev@users.noreply.github.com>
2021-10-10 01:39:47 +0300

    Update Kod.py
    Добавлена сумма форм для каждого Героя.
    Добавлен выбор текста.

--- Harry_Potter/Kod.py
+++ Harry_Potter/Kod.py
@@ -1,11 +1,24 @@
-import re 
+import re
 
+ans = int(input('Введите номер произведения: '))
+if ans == 1:
+    file = open('Garri_Potter_I_Dary_Smerti.txt','r')
+if ans == 2:
+    file = open('Garri-Potter-I-Prints-Polukrovka.txt','r')         # С этим текстом проблема
+if ans == 3:
+    file = open('Garri_Potter_I_Kubok_Ognya.txt','r') 
+if ans == 4:
+    file = open('Garri_Potter_I_Orden_Fenixa.txt','r') 
+if ans == 5:
+    file = open('Garri_Potter_I_Taynaya_Komnata.txt','r') 
+if ans == 6:
+    file = open('Garri_Potter_I_Uznik_Azkabana.txt','r') 
+if ans == 7:
+    file = open('Garri_Potter_I_Filosofsky_Kamen.txt','r')
+    
+f2 = file.read()                                                    # Открытие и Чтение Файла
 
-#file = open('Garri_Potter_I_Filosofsky_Kamen.txt','r')
-file = open('Garri_Potter_I_Dary_Smerti.txt','r')       # Открытие и Чтение Файла
-f2 = file.read()
-
-f2 = (f2.translate({ord(i): None for i in ','}))        #Убираем все лишние символы
+f2 = (f2.translate({ord(i): None for i in ','}))                    #Убираем все лишние символы
 f2 = (f2.translate({ord(i): None for i in '.'}))
 f2 = (f2.translate({ord(i): None for i in '-'}))
 f2 = (f2.translate({ord(i): None for i in '«'}))
@@ -17,26 +30,45 @@ f2 = (f2.translate({ord(i): None for i in ':'}))
 f2 = (f2.translate({ord(i): None for i in ')'}))
 f2 = (f2.translate({ord(i): None for i in '('}))
 
+text = f2.split()                                                   #Превращаем Текст В Массив
 
-text = f2.split()                                       #Превращаем Текст В Массив
 
 
-#name_forms = (re.findall(r'(Хагрид|Гермион|Дамблдор)[а|е|ы|у|ой|ом]', f2)) не работает и ладно #С помощью Регулярных Выржения ищем нужное
-name_forms = (re.findall(r'Дамблдор[а |у |ом|ы |ов|овский|ова]|Хагрид[а |ы |у |ом|ов|овский|ова]|Гермион[а |е |ы |у |ой|овский|ин]', f2))
+#С помощью Регулярных Выржения ищем нужное
+name_forms = (re.findall(r'Д[а|у]мбльдор[а |у |ом|ы |ов|овский|ова]|[Ха|О]грид[а |ы |у |ом|ов|овский|ова]|Гермион[а |е |ы |у |ой|овский|ин]', f2))
 
-for i in range (len(name_forms)):                       #Делаем все ячейки массива без заглавных букв
-    name_forms[i] = name_forms[i].lower()
 
+num_germ = (len(re.findall(r'Гермион[а |е |ы |у |ой|овский|ин]', f2)))          # Формы Имени Гермиона
+
+num_hagr = (len(re.findall(r'[Ха|О]грид[а |ы |у |ом|ов|овский|ова]', f2)))      # Формы Имени Хагрид (Огрид)
+
+num_damb = (len(re.findall(r'Д[а|у]мбльдор[а |у |ом|ы |ов|овский|ова]', f2)))  # Формы Имени Дамблдор (Думбльдор)
     
-name_forms_list = (list(set(name_forms)))               #Массив с неповторяющимися Формами имени
+print('')
+print('Инормация о формах имен Героев: ')
+
+print('Количество форм Имени Гермиона: ', (num_germ))
+
+print('Количество форм Имени Дамблдор (Думбльдор): ', (num_damb))
+
+print('Количество форм Имени Хагрид (Огрид):', (num_hagr))
+
+print('')
+
+name_forms_list = (list(set(name_forms)))                                       #Массив с неповторяющимися Формами имени
 name_forms_list.sort()
 
 
-for i in range (len(name_forms_list)):                  #Делаем все ячейки массива без заглавных букв
-    name_forms_list[i] = name_forms_list[i].lower() 
 
+for i in range (len(name_forms)):                                               #Делаем все ячейки массива без заглавных букв
+    name_forms[i] = name_forms[i].lower()
 
-for i in range (len(name_forms_list)):                  #Цикл, выводящий форму имени и количество этих форм
+for i in range (len(name_forms_list)):
+    name_forms_list[i] = name_forms_list[i].lower()
+
+print('Формы Имен и их количество: ')
+
+for i in range (len(name_forms_list)):                                          #Цикл, выводящий форму имени и количество этих форм
     print(name_forms_list[i])
     n = 0
     for k in range (len(name_forms)):
