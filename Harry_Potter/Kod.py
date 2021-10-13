f3dddb21dedf9140ea7e429e32e19b428adcae12
Gordey Zuev <58434126+GordeyZuev@users.noreply.github.com>
2021-10-13 13:58:09 +0300

    Update Kod.py

--- Harry_Potter/Kod.py
+++ Harry_Potter/Kod.py
@@ -1,20 +1,28 @@
 import re
 
+k1 = open('Garri_Potter_I_Dary_Smerti.txt','r') 
+k2 = open('Garri_Potter_I_Dary_Smerti.txt','r')
+k3 = open('Garri_Potter_I_Kubok_Ognya.txt','r') 
+k4 = open('Garri_Potter_I_Orden_Fenixa.txt','r')
+k5 = open('Garri_Potter_I_Taynaya_Komnata.txt','r') 
+k6 = open('Garri_Potter_I_Uznik_Azkabana.txt','r') 
+k7 = open('Garri_Potter_I_Filosofsky_Kamen.txt','r')
+
 ans = int(input('Введите номер произведения: '))
 if ans == 1:
-    file = open('Garri_Potter_I_Dary_Smerti.txt','r')                   # Не декодирует
+    file = k1                   # Не декодирует
 if ans == 2:
-    file = open('Garri-Potter-I-Prints-Polukrovka.txt','r')             # Не декодирует
+    file = k2                   # Не декодирует
 if ans == 3:
-    file = open('Garri_Potter_I_Kubok_Ognya.txt','r')                   # Декодирует
+    file = k3                   # Декодирует
 if ans == 4:
-    file = open('Garri_Potter_I_Orden_Fenixa.txt','r')                  # Декодирует
+    file = k4                   # Декодирует
 if ans == 5:
-    file = open('Garri_Potter_I_Taynaya_Komnata.txt','r')               # Декодирует
+    file = k5                   # Декодирует
 if ans == 6:
-    file = open('Garri_Potter_I_Uznik_Azkabana.txt','r')                # Декодирует
+    file = k6                   # Декодирует
 if ans == 7:
-    file = open('Garri_Potter_I_Filosofsky_Kamen.txt','r')              # Декодирует
+    file = k7                   # Декодирует
     
 f2 = file.read()                                                        # Открытие и Чтение Файла
 
@@ -30,19 +38,20 @@ f2 = (f2.translate({ord(i): None for i in ':'}))
 f2 = (f2.translate({ord(i): None for i in ')'}))
 f2 = (f2.translate({ord(i): None for i in '('}))
 
-
-#text = f2.split()                                                      # Превращаем Текст В Массив
 f2 = f2.lower()
 
 
 #С помощью Регулярных Выржения ищем нужное
-name_forms = (re.findall(r'д[а-я]{1,2}мбл[a-я]{0,2}дор[а-я]{0,10}|[ха,о]грид[а-я]{0,6}|гермион[а,е,ы,у,ой,ин,овский]', f2))
+name_forms = (re.findall(r'д[а,у]мбл[ ,ь]дор[а-я]{0,6}|[ха,о]грид[а-я]{0,6}|гермион[а-я]{0,6}', f2))
 
-num_germ = (len(re.findall(r'гермион[а,е,ы,у,ой,ин,овский]', f2)))              # Формы Имени Гермиона
+
+#num_germ = (len(re.findall(r'гермион[ ,а,е,ы,у,ой,ин,овский]', f2)))
+# 
+num_germ = (len(re.findall(r'гермион[а-я]{0,6}', f2)))              # Формы Имени Гермиона
 
 num_hagr = (len(re.findall(r'[ха,о]грид[а-я]{0,6}', f2)))                       # Формы Имени Хагрид (Огрид)
 
-num_damb = (len(re.findall(r'д[а,у]мбл[а-я]{0,2}дор[а-я]{0,10}', f2)))          # Формы Имени Дамблдор (Думбльдор)
+num_damb = (len(re.findall(r'д[а,у]мбл[ ,ь]дор[а-я]{0,6}', f2)))          # Формы Имени Дамблдор (Думбльдор)
     
     
 print('')
